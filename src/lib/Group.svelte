d5c35787f63ee4ee8b11b69731bc993c54a28494
Rich Harris <richard.a.harris@gmail.com>
2021-05-25 18:06:02 -0400

    -> v0.0.5

--- src/lib/Group.svelte
+++ src/lib/Group.svelte
@@ -4,19 +4,44 @@
 
 </script>
 
-<section>
-	<h1>{label}</h1>
+<details open>
+	<summary>{label}</summary>
 
 	<div class="group">
 		{#each knobs as { component, ...knob }}
 			<svelte:component this={component} {...knob}/>
 		{/each}
 	</div>
-</section>
+</details>
 
 <style>
-	h1 {
+	summary {
+		position: relative;
 		font-size: 1em;
+		padding: 0.5rem 0 0.5rem 1.5rem;
+		list-style: none;
+		user-select: none;
+	}
+
+	summary::before {
+		content: '';
+		position: absolute;
+		left: 2px;
+		top: calc(50% - 4px);
+		transform-origin: 25% 50%;
+		width: 0;
+		height: 0;
+		border: 4px solid transparent;
+		border-left-width: 5px;
+		border-right-width: 5px;
+		border-top-width: 4px;
+		border-bottom-width: 4px;
+		border-left-color: currentColor;
+		transition: transform 0.1s ease-out;
+	}
+
+	[open] summary::before {
+		transform: rotate(90deg);
 	}
 
 	.group {
