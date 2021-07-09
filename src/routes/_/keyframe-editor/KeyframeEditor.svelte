f1f3e58d7fa7d23901c9a6bef5ae50ce2f13f456
Rich Harris <richard.a.harris@gmail.com>
2021-07-09 19:11:27 -0400

    various

--- src/routes/_/keyframe-editor/KeyframeEditor.svelte
+++ src/routes/_/keyframe-editor/KeyframeEditor.svelte
@@ -98,122 +98,143 @@
 	});
 </script>
 
-<span>{name}</span>
+<div class="keyframe-editor" tabindex="0" on:keydown={e => {
+	console.log(e);
+}}>
+	<span>{name}</span>
 
-<div
-	class="keyframe-editor"
-	style="cursor: {cursor}"
-	bind:clientWidth={w}
-	bind:clientHeight={h}
-	use:drag={{
-		start: drag => {
-			const selection = select(drag.start.x, drag.start.y);
+	<div
+		class="canvas-container"
+		style="cursor: {cursor}"
+		bind:clientWidth={w}
+		bind:clientHeight={h}
+		use:drag={{
+			start: drag => {
+				const selection = select(drag.start.x, drag.start.y);
 
-			drag.context.selection = selection;
+				drag.context.selection = selection;
+				drag.context.multiplier = {
+					x: (bounds.x2 - bounds.x1) / (canvas.offsetWidth - padding * 2),
+					y: (bounds.y2 - bounds.y1) / (canvas.offsetHeight - padding * 2)
+				}
 
-			// TODO support multiple selections
-			selected_points = [];
+				// TODO support multiple selections
+				selected_points = [];
 
-			if (selection) {
-				drag.context.start = [...selection.point];
+				if (selection) {
+					drag.context.start = [...selection.point];
 
-				const prev = selection.track.points[selection.index - 1];
-				const next = selection.track.points[selection.index + 1];
+					const prev = selection.track.points[selection.index - 1];
+					const next = selection.track.points[selection.index + 1];
 
-				drag.context.bounds = {
-					x1: prev ? prev[0] + EPSILON : -Infinity,
-					x2: next ? next[0] - EPSILON : +Infinity
-				};
+					drag.context.bounds = {
+						x1: prev ? prev[0] + EPSILON : -Infinity,
+						x2: next ? next[0] - EPSILON : +Infinity
+					};
 
-				selected_points = [selection.point];
+					selected_points = [selection.point];
+				}
+
+				cursor = selection ? 'move' : 'grabbing';
+			},
+			move: (drag, e) => {
+				if (drag.context.selection) {
+					let dx = drag.x * drag.context.multiplier.x;
+					let dy = drag.y * drag.context.multiplier.y;
+
+					const { point } = drag.context.selection;
+					const { x1, x2 } = drag.context.bounds;
+
+					if (e.shiftKey) {
+						if (Math.abs(drag.x) > Math.abs(drag.y)) {
+							dy = 0;
+						} else {
+							dx = 0;
+						}
+					}
+
+					// TODO move handles
+					point[0] = yootils.clamp(drag.context.start[0] + dx, x1, x2);
+					point[1] = drag.context.start[1] - dy;
+
+					value = value;
+				} else {
+					const dx = drag.dx * drag.context.multiplier.x;
+					const dy = drag.dy * drag.context.multiplier.y;
+
+					bounds.x1 -= dx;
+					bounds.x2 -= dx;
+					bounds.y1 += dy;
+					bounds.y2 += dy;
+				}
+			},
+			end: drag => {
+				cursor = 'grab';
 			}
+		}}
+		on:pointerdown={e => {
 
-			cursor = selection ? 'move' : 'grabbing';
-		},
-		move: (drag, e) => {
-			let dx = drag.x * (bounds.x2 - bounds.x1) / (canvas.offsetWidth - padding * 2);
-			let dy = drag.y * (bounds.y2 - bounds.y1) / (canvas.offsetHeight - padding * 2);
+		}}
+		on:pointermove={e => {
+			if (!e.buttons) {
+				cursor = select(e.clientX, e.clientY) ? 'move' : 'grab';
+			}
+		}}
+		on:wheel={e => {
+			if (e.metaKey || e.shiftKey || e.altKey) {
+				e.preventDefault();
 
-			if (drag.context.selection) {
-				const { point } = drag.context.selection;
-				const { x1, x2 } = drag.context.bounds;
+				const bcr = canvas.getBoundingClientRect();
 
-				if (e.shiftKey) {
-					if (Math.abs(drag.x) > Math.abs(drag.y)) {
-						dy = 0;
-					} else {
-						dx = 0;
+				if (e.altKey) {
+					const dx = e.wheelDeltaX * (bounds.x2 - bounds.x1) / bcr.width;
+					const dy = e.wheelDeltaY * (bounds.y2 - bounds.y1) / bcr.height;
+
+					bounds.x1 -= dx * 0.25;
+					bounds.x2 -= dx * 0.25;
+					bounds.y1 += dy * 0.25;
+					bounds.y2 += dy * 0.25;
+				} else {
+					const px = (e.clientX - bcr.left) / bcr.width;
+					const py = (e.clientY - bcr.top) / bcr.height;
+					const cx = bounds.x1 + px * (bounds.x2 - bounds.x1);
+					const cy = bounds.y1 + py * (bounds.y2 - bounds.y1);
+
+					const amount = Math.pow(Math.exp(-e.wheelDeltaY), 0.01);
+
+					if (e.metaKey) {
+						// zoom x
+						bounds.x1 = cx - amount * (cx - bounds.x1);
+						bounds.x2 = cx + amount * (bounds.x2 - cx);
+					}
+
+					if (e.shiftKey) {
+						// zoom y
+						bounds.y1 = cy - amount * (cy - bounds.y1);
+						bounds.y2 = cy + amount * (bounds.y2 - cy);
 					}
 				}
-
-				// TODO move handles
-				point[0] = yootils.clamp(drag.context.start[0] + dx, x1, x2);
-				point[1] = drag.context.start[1] - dy;
-
-				value = value;
-			} else {
-				bounds.x1 -= dx;
-				bounds.x2 -= dx;
-				bounds.y1 += dy;
-				bounds.y2 += dy;
 			}
-		},
-		end: drag => {
-			cursor = 'grab';
-		}
-	}}
-	on:pointerdown={e => {
+		}}
+	>
+		<canvas bind:this={canvas}></canvas>
+	</div>
 
-	}}
-	on:pointermove={e => {
-		if (!e.buttons) {
-			cursor = select(e.clientX, e.clientY) ? 'move' : 'grab';
-		}
-	}}
-	on:wheel={e => {
-		if (e.metaKey || e.shiftKey || e.altKey) {
-			e.preventDefault();
-
-			const bcr = canvas.getBoundingClientRect();
-
-			if (e.altKey) {
-				const dx = e.wheelDeltaX * (bounds.x2 - bounds.x1) / bcr.width;
-				const dy = e.wheelDeltaY * (bounds.y2 - bounds.y1) / bcr.height;
-
-				bounds.x1 -= dx * 0.25;
-				bounds.x2 -= dx * 0.25;
-				bounds.y1 += dy * 0.25;
-				bounds.y2 += dy * 0.25;
-			} else {
-				const px = (e.clientX - bcr.left) / bcr.width;
-				const py = (e.clientY - bcr.top) / bcr.height;
-				const cx = bounds.x1 + px * (bounds.x2 - bounds.x1);
-				const cy = bounds.y1 + py * (bounds.y2 - bounds.y1);
-
-				const amount = Math.pow(Math.exp(-e.wheelDeltaY), 0.01);
-
-				if (e.metaKey) {
-					// zoom x
-					bounds.x1 = cx - amount * (cx - bounds.x1);
-					bounds.x2 = cx + amount * (bounds.x2 - cx);
-				}
-
-				if (e.shiftKey) {
-					// zoom y
-					bounds.y1 = cy - amount * (cy - bounds.y1);
-					bounds.y2 = cy + amount * (bounds.y2 - cy);
-				}
-			}
-		}
-	}}
->
-	<canvas bind:this={canvas}></canvas>
+	<button on:click={fit}>fit</button>
 </div>
 
-<button on:click={fit}>fit</button>
-
 <style>
-	.keyframe-editor {
+	.keyframe-editor:focus-within {
+		position: relative;
+		margin: -2px;
+		padding: 2px;
+		outline: none;
+		border-radius: var(--border-radius);
+		box-shadow: 0 0 0 2px hsla(var(--hue), 11%, 40%, 0.4);
+		z-index: 2;
+	}
+
+	.canvas-container {
 		width: 100%;
 		height: 200px;
 		border-radius: var(--border-radius); /* TODO namespace and catalog these values */
@@ -223,7 +244,7 @@
 		margin: 0 0 0.5rem 0;
 	}
 
-	.keyframe-editor::before {
+	.canvas-container::before {
 		content: '';
 		position: absolute;
 		width: 100%;
